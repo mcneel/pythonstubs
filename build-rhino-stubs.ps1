9908a9a876cf915e096997bda577868c7d581350
Steve Baer <steve@mcneel.com>
2019-09-13 22:43:55 -0700

    rename app and lib

--- build-rhino-stubs.ps1
+++ build-rhino-stubs.ps1
@@ -5,13 +5,13 @@ $rhinobase = "C:\Program Files\Rhino WIP"
 $rhinoplugins = "$rhinobase\Plug-ins"
 $rhinosystem = "$rhinobase\System"
 
-cd "builder\bin\Release x64"
+cd "builder\bin\Release"
 
-./pystubsbuilder.exe --dest="$stubsdest\Eto" --search=$rhinosystem --postfix="-stubs" "$rhinosystem\Eto.dll"
-./pystubsbuilder.exe --dest="$stubsdest\Rhino" --search=$rhinosystem --postfix="-stubs" "$rhinosystem\RhinoCommon.dll"
-./pystubsbuilder.exe --dest="$stubsdest\Grasshopper" --search=$rhinosystem --postfix="-stubs" "$rhinoplugins\Grasshopper\Grasshopper.dll"
-./pystubsbuilder.exe --dest="$stubsdest\GH_IO" --search=$rhinosystem --postfix="-stubs" "$rhinoplugins\Grasshopper\GH_IO.dll"
-./pystubsbuilder.exe --dest="$stubsdest\GH_Util" --search=$rhinosystem --postfix="-stubs" "$rhinoplugins\Grasshopper\GH_Util.dll"
+./PyStubbler.exe --dest="$stubsdest\Eto" --search=$rhinosystem --postfix="-stubs" "$rhinosystem\Eto.dll"
+./PyStubbler.exe --dest="$stubsdest\Rhino" --search=$rhinosystem --postfix="-stubs" "$rhinosystem\RhinoCommon.dll"
+./PyStubbler.exe --dest="$stubsdest\Grasshopper" --search=$rhinosystem --postfix="-stubs" "$rhinoplugins\Grasshopper\Grasshopper.dll"
+./PyStubbler.exe --dest="$stubsdest\GH_IO" --search=$rhinosystem --postfix="-stubs" "$rhinoplugins\Grasshopper\GH_IO.dll"
+./PyStubbler.exe --dest="$stubsdest\GH_Util" --search=$rhinosystem --postfix="-stubs" "$rhinoplugins\Grasshopper\GH_Util.dll"
 
 # back to where started
 cd $mypath
---
+++ builder/PyStubblerLib/BuildConfig.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace PyStubblerLib
+{
+  public class BuildConfig
+  {
+    public string Prefix { get; set; } = string.Empty;
+    public string Postfix { get; set; } = string.Empty;
+    public bool DestPathIsRoot { get; set; } = false;
+  }
+}
